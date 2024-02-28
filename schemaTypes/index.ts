73843632cba462be260396e353b4028a05f8744c
Gerelsuren <gerlee.gerlee71@gmail.com>
2024-02-28 10:54:05 +0800

    eCommerce projectiin cms uusgelee

---
+++ schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import basket from './basket'
+export const schemaTypes = [post, author, basket, category, blockContent]
+/* eCommerce-cms-react-18.sanity.studio */
