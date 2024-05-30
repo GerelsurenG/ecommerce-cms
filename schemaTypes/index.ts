208cbe970e2c32630ffe15ad38b9c46493af6a7c
Gerelsuren <gerlee.gerlee71@gmail.com>
2024-05-30 16:06:43 +0800

    action log uusgew

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import basket from './basket'
-export const schemaTypes = [post, author, basket, category, blockContent]
+import action from './actionLog'
+export const schemaTypes = [post, author, basket, category, blockContent, action]
 /* eCommerce-cms-react-18.sanity.studio */
